4c6041527f8acbf1b1a6f8b9a560e09496f81735
Chuckki3 <143392252+Chuckki3@users.noreply.github.com>
2026-03-27 17:22:50 +0100

    Create 01_create_clean_table.sql
    Creates a standardized and cleaned version of the raw transaction dataset to serve as the foundation for downstream feature engineering.

---
+++ sql/01_create_clean_table.sql
@@ -0,0 +1,16 @@
+-- Step 1: Create clean transactions table
+
+CREATE OR REPLACE TABLE `dotted-rookery-490711-i3.hostfi.transactions_clean` AS
+SELECT
+  step,
+  type,
+  amount,
+  nameOrig,
+  oldbalanceOrg,
+  newbalanceOrig,
+  nameDest,
+  oldbalanceDest,
+  newbalanceDest,
+  isFraud,
+  isFlaggedFraud
+FROM `dotted-rookery-490711-i3.Paysim_unbroken.transactions_total`;
